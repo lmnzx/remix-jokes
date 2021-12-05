9ad012314a8bc6f4b362637b77b31cb5379e422a
Sayan Mallick <snyxmk@gmail.com>
2021-12-05 23:37:23 +0530

    Routes

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,10 @@
+import type { LinksFunction } from "remix";
+import styleUrl from "../styles/index.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: styleUrl }];
+};
+
 export default function IndexRoute() {
   return (
     <div>
